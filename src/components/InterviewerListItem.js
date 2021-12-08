3ce4ee225693f89df2b676cc03ff0c4658f46f27
aliy0012 <alikamal415@yahoo.com>
2021-12-08 18:58:01 -0500

    add conditional render the interviewers name

--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,15 +1,21 @@
 import React from "react";
-import "components/InterviewerListItem.scss"
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
 
 export default function InterviewerListItem(props) {
+  //conditionally adding classes
+  const interviewClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
   return (
-    <li className="interviewers__item">
-  <img
-    className="interviewers__item-image"
-    src="https://i.imgur.com/LpaY82x.png"
-    alt="Sylvia Palmer"
-  />
-  Sylvia Palmer
-</li>
+    <li className={interviewClass} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
   );
 }
