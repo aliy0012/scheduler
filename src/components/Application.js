f33a08f88a64c1882e1fb6ce52d392abdc08c5a3
aliy0012 <alikamal415@yahoo.com>
2021-12-07 22:42:49 -0500

    set default day to Monday

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,4 +1,4 @@
-import React from "react";
+
 
 import "components/Application.scss";
 import DayList from "./DayList";
@@ -23,6 +23,7 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState("Monday");
   return (
     <main className="layout">
       <section className="sidebar">
