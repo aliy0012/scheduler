7848c374919edbd7c0e9b3d829b9176656fcef40
aliy0012 <alikamal415@yahoo.com>
2021-12-07 22:21:20 -0500

    add DAyList component stories

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,8 +1,19 @@
 import React from "react";
 import DayListItem from "./DayListItem";
 
-export default function DayList(props {
+export default function DayList(props) {
+  // mapping over days array
   return (
-    <ul></ul>
+    <ul>
+      {props.days.map((day) => (
+        <DayListItem
+          key={day.id}
+          name={day.name}
+          spots={day.spots}
+          selected={day.name === props.day}
+          setDay={() => props.setDay(day.name)}
+        />
+      ))}
+    </ul>
   );
-})
+}
