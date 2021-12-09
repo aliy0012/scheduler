44d07c389f5fbd55e605d6558c014c80c5f8a498
aliy0012 <alikamal415@yahoo.com>
2021-12-09 17:19:49 -0500

    import header, show and empty components

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,22 +3,48 @@ import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
-  const [student, setStudent] = useState(props.student || "");
+  const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
+  //function to clean interviewer
+  const reset = () => {
+    setName("");
+    setInterviewer(null);
+  };
+
+
+  // creating cancel function to cancel an appointment and return the previous state
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  
+  
+  const validate = () => {
+    if (name === "") {
+      setError("The name cannot be left blank.");
+      return;
+    } else if (interviewer === null) {
+      setError("An interviewer must be selected.");
+      return;
+    } else {
+      setError("");
+      return props.onSave(name, interviewer);
+    }
+  };
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form onSubmit={(event) => event.preventDefault()} autoComplete="off">
           <input
+            data-testid="student-name-input"
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-          This must be a controlled component
-          your code goes here
-        */
             value={name}
             onChange={(event) => {
               setName(event.target.value);
