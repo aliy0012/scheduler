6d81fd7b55cf79ae47d0977eaa8e00acc7cbdb64
aliy0012 <alikamal415@yahoo.com>
2021-12-09 16:43:52 -0500

    add default state

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -3,8 +3,50 @@ import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
-  return (
-    
-  );
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
 
+  return (
+    <main className="appointment__card appointment__card--create">
+      <section className="appointment__card-left">
+        <form autoComplete="off">
+          <input
+            className="appointment__create-input text--semi-bold"
+            name="name"
+            type="text"
+            placeholder="Enter Student Name"
+            /*
+          This must be a controlled component
+          your code goes here
+        */
+            value={name}
+            onChange={(event) => {
+              setName(event.target.value);
+              if (event.target.value !== "") {
+                setError(false);
+              }
+            }}
+          />
+        </form>
+        {error && (
+          <section className="appointment__validation">{error}</section>
+        )}
+        <InterviewerList
+          interviewers={props.interviewers}
+          value={interviewer}
+          onChange={setInterviewer}
+        />
+      </section>
+      <section className="appointment__card-right">
+        <section className="appointment__actions">
+          <Button danger onClick={cancel}>
+            Cancel
+          </Button>
+          <Button confirm onClick={() => validate()}>
+            Save
+          </Button>
+        </section>
+      </section>
+    </main>
+  );
 }
