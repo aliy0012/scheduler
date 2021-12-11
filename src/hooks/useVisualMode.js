4118f9292c282a9f58ef65d81a354a8305f7a6a4
aliy0012 <alikamal415@yahoo.com>
2021-12-11 13:04:58 -0500

    build custom hooks

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,5 +1,29 @@
+
+import { useState } from "react";
+
+
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
 
-  return { mode };
+  
+
+
+const transition = (newMode, replace = false) => {
+  if (replace) {
+    setMode(newMode);
+    setHistory((prev) => [...prev]);
+  } else {
+    setMode(newMode);
+    setHistory((prev) => [...prev, newMode]);
+  }
+};
+
+const back = () => {
+  if (history.length !== 0) {
+    history.pop();
+    setMode(history[history.length - 1]);
+  }
+};
+
+return { mode, transition, back };
 }
