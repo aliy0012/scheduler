304af93501fa7e67119d5ed08eef2fc246d4a6d3
aliy0012 <alikamal415@yahoo.com>
2021-12-11 01:13:03 +0000

    add useEffect

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,6 +5,8 @@ import DayList from "./DayList";
 import React, { useState } from "react";
 import Appointment from "components/Appointment";
 
+import axios from "axios";
+
 const appointments = [
   {
     id: 1,
@@ -44,23 +46,12 @@ const appointments = [
   }
 ];
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+const [days, setDays] = useState([]);
+
+useEffect(() => {
+  axios.get("/api/days")
+}, []);
+
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
