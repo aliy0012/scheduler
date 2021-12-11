47682d7b16d2786f38403d7068a93a775d154bca
aliy0012 <alikamal415@yahoo.com>
2021-12-11 01:46:56 +0000

    update application compnent

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,52 +7,14 @@ import Appointment from "components/Appointment";
 
 import axios from "axios";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
-
-
-
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-
-  const [days, setDays] = useState([]);
+ 
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
 useEffect(() => {
   axios.get("/api/days")
@@ -70,7 +32,7 @@ useEffect(() => {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+        <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
