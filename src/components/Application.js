4402b0ad28cda57c1f708587ccd5c1336b07c9d0
aliy0012 <alikamal415@yahoo.com>
2021-12-14 15:14:30 -0500

    application.js bug fix

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,91 +1,36 @@
-
-
+import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
-import React, { useState, useEffect } from "react";
 import Appointment from "components/Appointment";
 
-import axios from "axios";
+import {
+  getAppointmentsForDay,
+  getInterview,
+  getInterviewersForDay,
+} from "helpers/selectors";
 
-import {getAppointmentsForDay, getInterview} from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 
 export default function Application(props) {
+  const { state, setDay, bookInterview, cancelInterview } =
+    useApplicationData();
 
-  const [state, setState] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {}
-  });
-
-  const dailyAppointments = getAppointmentsForDay(state, day);
-
-  const setDay = (day) => setState({ ...state, day });
-
-  const schedule = appointments.map((appointment) => {
-  const interview = getInterview(state, appointment.interview);
-
-  setState({
-    ...state,
-    appointments
-  });
-
-  function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-  
-    function save(name, interviewer) {
-      const interview = {
-        student: name,
-        interviewer,
-      };
-      transition(SAVING);
-      props
-        .bookInterview(props.id, interview)
-        .then(() => transition(SHOW))
-        .catch(() => transition(ERROR_SAVE, true));
+  const dailyInverviewers = getInterviewersForDay(state, state.day);
+  const dailyAppointments = getAppointmentsForDay(state, state.day).map(
+    (appointment) => {
+      return (
+        <Appointment
+          key={appointment.id}
+          id={appointment.id}
+          time={appointment.time}
+          interviewers={dailyInverviewers}
+          interview={getInterview(state, appointment.interview)}
+          bookInterview={bookInterview}
+          cancelInterview={cancelInterview}
+        />
+      );
     }
-
-  function deleteAppointment() {
-    transition(DELETE, true);
-    props
-      .cancelInterview(props.id)
-      .then(() => transition(EMPTY))
-      .catch(() => transition(ERROR_DELETE, true));
-  }
-
-
-  return (
-    <Appointment
-      key={appointment.id}
-      id={appointment.id}
-      time={appointment.time}
-      interview={interview}
-    />
   );
-
-
-  useEffect(() => {
-    Promise.all([
-      axios.get("/api/days"),
-      axios.get("/api/appointments"),
-      axios.get("/api/interviewers"),
-    ]).then((response) => {
-      setState((prev) => ({
-        ...prev,
-        days: response[0].data,
-        appointments: response[1].data,
-        interviewers: response[2].data,
-      }));
-    });
-  }, []);
-
-  //looping appointments mock data 
 
   return (
     <main className="layout">
@@ -97,7 +42,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList days={state.days} day={state.day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -106,10 +51,9 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {dailyAppointments.map((appointment) => (
-          <Appointment key={appointment.id} {...appointment} />
-        ))}
-        <Appointment id="last" time="5pm" />
+        {dailyAppointments}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
+}
